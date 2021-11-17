04c81dd4d0dcf8a90f88d1730591ce7bae084f9f
Marcell Feher <sw0rdf1sh@eng.au.dk>
2021-11-17 15:26:40 +0000

    fix

--- src/lib/import_export/binary/binary_writer.cpp
+++ src/lib/import_export/binary/binary_writer.cpp
@@ -278,31 +278,31 @@ void BinaryWriter::_write_segment(const FrameOfReferenceSegment<int32_t>& frame_
                             frame_of_reference_segment.offset_values());
 }
 
-template <>
-void BinaryWriter::_write_segment(const GddSegment<int32_t>& frame_of_reference_segment,
+template <typename T>
+void BinaryWriter::_write_segment(const GddSegment<T>& gdd_segment,
                                   bool column_is_nullable, std::ofstream& ofstream) {
   export_value(ofstream, EncodingType::GDD);
 
   // GDDTODO: write me
 
   // Write attribute vector compression id
-  const auto compressed_vector_type_id = _compressed_vector_type_id<int32_t>(frame_of_reference_segment);
+  const auto compressed_vector_type_id = _compressed_vector_type_id<int32_t>(gdd_segment);
   export_value(ofstream, compressed_vector_type_id);
 
   // Write number of blocks and block minima
-  export_value(ofstream, static_cast<uint32_t>(frame_of_reference_segment.block_minima().size()));
-  export_values(ofstream, frame_of_reference_segment.block_minima());
+  export_value(ofstream, static_cast<uint32_t>(gdd_segment.block_minima().size()));
+  export_values(ofstream, gdd_segment.block_minima());
 
   // Write flag if optional NULL value vector is written
-  export_value(ofstream, static_cast<BoolAsByteType>(frame_of_reference_segment.null_values().has_value()));
-  if (frame_of_reference_segment.null_values()) {
+  export_value(ofstream, static_cast<BoolAsByteType>(gdd_segment.null_values().has_value()));
+  if (gdd_segment.null_values()) {
     // Write NULL values
-    export_values(ofstream, *frame_of_reference_segment.null_values());
+    export_values(ofstream, *gdd_segment.null_values());
   }
 
   // Write offset values
-  _export_compressed_vector(ofstream, *frame_of_reference_segment.compressed_vector_type(),
-                            frame_of_reference_segment.offset_values());
+  _export_compressed_vector(ofstream, *gdd_segment.compressed_vector_type(),
+                            gdd_segment.offset_values());
 }
 
 template <typename T>
