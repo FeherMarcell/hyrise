7dd041cdcb24face13aa5b43db5623b415a3d015
Marcell Feher <sw0rdf1sh@eng.au.dk>
2021-11-17 12:45:08 +0000

    fix

--- src/lib/storage/gdd_segment/gdd_encoder.hpp
+++ src/lib/storage/gdd_segment/gdd_encoder.hpp
@@ -85,9 +85,12 @@ class GddEncoder : public SegmentEncoder<GddEncoder> {
         const auto this_value_block_end = value_block_it;
 
         if (block_contains_values) {
+          // GDDTODO: std::make_unsigned is not compatible with float and double, which are supported by Gdd
+          /*
           // Make sure that the largest offset fits into uint32_t (required for vector compression).
           Assert(static_cast<std::make_unsigned_t<T>>(max_value - min_value) <= std::numeric_limits<uint32_t>::max(),
                  "Value range in block must fit into uint32_t.");
+          */
         }
 
         block_minima.push_back(min_value);
