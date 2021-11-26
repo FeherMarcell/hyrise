74c242e0dd9a966ea26ac60c8c875b66c09c1dcd
Marcell Feher <sw0rdf1sh@eng.au.dk>
2021-11-26 16:52:03 +0000

    fix?

--- src/lib/storage/gdd_segment.cpp
+++ src/lib/storage/gdd_segment.cpp
@@ -9,26 +9,26 @@
 
 namespace opossum {
 
-template <typename T>
-GddSegment<T>::GddSegment(const std::shared_ptr<const pmr_vector<T>>& values,
+template <typename T, typename U>
+GddSegment<T, U>::GddSegment(const std::shared_ptr<const pmr_vector<T>>& values,
                           const std::shared_ptr<const pmr_vector<bool>>& null_values)
     : AbstractEncodedSegment(data_type_from_type<T>()),
       _values{values},
       _null_values{null_values} {}
 
-template <typename T>
-std::shared_ptr<const pmr_vector<T>> GddSegment<T>::values() const {
+template <typename T, typename U>
+std::shared_ptr<const pmr_vector<T>> GddSegment<T, U>::values() const {
   return _values;
 }
 
-template <typename T>
-std::shared_ptr<const pmr_vector<bool>> GddSegment<T>::null_values() const {
+template <typename T, typename U>
+std::shared_ptr<const pmr_vector<bool>> GddSegment<T, U>::null_values() const {
   return _null_values;
 }
 
 
-template <typename T>
-AllTypeVariant GddSegment<T>::operator[](const ChunkOffset chunk_offset) const {
+template <typename T, typename U>
+AllTypeVariant GddSegment<T, U>::operator[](const ChunkOffset chunk_offset) const {
   PerformanceWarning("operator[] used");
   const auto typed_value = get_typed_value(chunk_offset);
   if (!typed_value) {
@@ -37,35 +37,35 @@ AllTypeVariant GddSegment<T>::operator[](const ChunkOffset chunk_offset) const {
   return *typed_value;
 }
 
-template <typename T>
-ChunkOffset GddSegment<T>::size() const {
+template <typename T, typename U>
+ChunkOffset GddSegment<T, U>::size() const {
   return values.size();
 }
 
-template <typename T>
-std::shared_ptr<AbstractSegment> GddSegment<T>::copy_using_allocator(
+template <typename T, typename U>
+std::shared_ptr<AbstractSegment> GddSegment<T, U>::copy_using_allocator(
     const PolymorphicAllocator<size_t>& alloc) const {
   auto new_values = std::make_shared<pmr_vector<T>>(*_values, alloc);
   auto new_null_values = std::make_shared<pmr_vector<bool>>(*_null_values, alloc);
 
-  auto copy = std::make_shared<GddSegment<T>>(new_values, new_null_values);
+  auto copy = std::make_shared<GddSegment<T, U>>(new_values, new_null_values);
   copy->access_counter = access_counter;
 
   return copy;
 }
 
-template <typename T>
-size_t GddSegment<T>::memory_usage(const MemoryUsageCalculationMode mode) const {
+template <typename T, typename U>
+size_t GddSegment<T, U>::memory_usage(const MemoryUsageCalculationMode mode) const {
   return values.size() * sizeof(typename decltype(_values)::element_type::value_type) + null_values.size();
 }
 
-template <typename T>
-EncodingType GddSegment<T>::encoding_type() const {
+template <typename T, typename U>
+EncodingType GddSegment<T, U>::encoding_type() const {
   return EncodingType::GDD;
 }
 
-template <typename T>
-std::optional<CompressedVectorType> GddSegment<T>::compressed_vector_type() const {
+template <typename T, typename U>
+std::optional<CompressedVectorType> GddSegment<T, U>::compressed_vector_type() const {
   return std::nullopt;
 }
 
