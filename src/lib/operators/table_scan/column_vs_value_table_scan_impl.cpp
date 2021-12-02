d8f60544e83bd39a44d90a483b9b30e5ed9f68d0
Marcell Feher <sw0rdf1sh@eng.au.dk>
2021-12-02 10:31:07 +0000

    try

--- src/lib/operators/table_scan/column_vs_value_table_scan_impl.cpp
+++ src/lib/operators/table_scan/column_vs_value_table_scan_impl.cpp
@@ -48,7 +48,9 @@ void ColumnVsValueTableScanImpl::_scan_non_reference_segment(
     }
   }
 
-  if (const auto* gdd_segment = dynamic_cast<const GddSegment*>(&segment)){
+  using ColumnType = typename _in_table->column_data_type(column_id);
+
+  if (const auto* gdd_segment = dynamic_cast<const GddSegment<ColumnType>*>(&segment)){
     _scan_gdd_segment(*gdd_segment, chunk_id, matches, position_filter);
   }
   else if (const auto* dictionary_segment = dynamic_cast<const BaseDictionarySegment*>(&segment)) {
